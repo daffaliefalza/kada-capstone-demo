cb7fa1ed792b4bbf86e320d5c79b4fcf48e3b87e
foxxesource <bintang.duinata31@gmail.com>
2025-07-26 10:10:17 +0700

    feat: first project initialize

---
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HERO_IMG from './assets/hero-img.png'
+import { APP_FEATURES } from '../utils/data'
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
