9e6cd612583a0b342a375c89fe4249d524946562
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-27 14:05:37 +0700

    feat: update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const connectDB = require("./config/db.js");
+const app = express();
+
+// Middleware for cors
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// routes
+
+// serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Start server
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
