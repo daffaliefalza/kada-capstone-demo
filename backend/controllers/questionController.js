f93d8fab06c5d36af044649791264a3890f0cd88
Hera <shynnyshaquille@gmail.com>
2025-07-28 16:19:02 +0700

    feat: Creating Question APIs (Add Question to Section, Pin a Question)

---
+++ backend/controllers/questionController.js
@@ -0,0 +1,83 @@
+const Question = require("../models/Question");
+const Session = require("../models/Session");
+
+//@desc     Add additional question to an existing session
+//@route    POST /api/question/add
+//@access   Private
+exports.addQuestionsToSession = async (req, res) => {
+    try {
+        const { sessionId, questions } = req.body;
+
+        if (!sessionId || !questions || !Array.isArray(questions)) {
+            return res.status(400).json({ message: "invalid input data" }); 
+        }
+
+        const session = await Session.findById(sessionId);
+        if (!session) {
+            return res.status(404).json({ message: "Session not found" });
+        }
+
+        //Create new questions
+        const createdQuestions = await Question.insertMany(
+            questions.map((q) => ({
+                session: sessionId,
+                question: q.question,
+                answer: q.answer,
+            }))
+        );
+
+        // Update session to include new questions IDs
+        session.questions.push(...createdQuestions.map((q) => q._id));
+        await session.save();
+
+        res.status(201).json(createdQuestions);
+    }catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+    
+};
+
+//@desc     Pin or unpin a question
+//@route    POST /api/question/pin
+//@access   Private
+exports.togglePinQuestion = async (req, res) => {
+    try{
+     const question = await Question.findById(req.params.id);
+
+     if (!question) {
+        return res
+        .status(404)
+        .json({ success:false, message: "Question not found" });
+     }
+
+     question.isPinned = !question.isPinned;
+     await question.save();
+
+     res.status(200).json({ success:true, question });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
+
+//@desc     Update a note for a question
+//@route    POST /api/question/:id/note
+//@access   Private
+exports.updateQuestionNote = async (req, res) => {
+    try{
+        const { note } = req.body;
+        const question = await Question.findById(req.params.id);
+        
+        if (!question) {
+            return res
+            .status(404)
+            .json({ success:false, message: "Question not found" });
+        }
+
+        question.note = note || "";
+        await question.save();
+
+        res.status(200).json({ success:true, question });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+}
