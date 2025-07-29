c4103ade719dca5dc379308334957b12c13462ec
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-30 06:07:07 +0700

    fix: import in dashboard

--- frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
+++ frontend/interview-prep-ai/src/pages/Home/Dashboard.jsx
@@ -6,8 +6,8 @@ import DashboardLayout from "../../components/Layouts/DashboardLayout";
 import axiosInstance from "../../utils/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
 import SummaryCard from "../../components/Cards/SummaryCard";
-
 const Dashboard = () => {
   const navigate = useNavigate();
   const [openCreateModal, setOpenCreateModal] = useState(false);
@@ -21,6 +21,7 @@ const Dashboard = () => {
   const fetchAllSessions = async () => {
     try {
       const response = await axiosInstance.get(API_PATHS.SESSION.GET_ALL);
+      // console.log("Fetched sessions:", response.data); // 🔍 check this!
       setSessions(response.data);
     } catch (error) {
       console.error("Error fetching session data:", error);
@@ -33,11 +34,13 @@ const Dashboard = () => {
     fetchAllSessions();
   }, []);
 
+  console.log("Sessions data:", sessions);
+
   return (
     <DashboardLayout>
       <div className="container mx-auto pt-4 pb-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-4">
-          {sessions?.map((data, index) => {
+          {sessions?.map((data, index) => (
             <SummaryCard
               key={data?._id}
               colors={CARD_BG[index % CARD_BG.length]}
@@ -48,13 +51,13 @@ const Dashboard = () => {
               description={data?.description || ""}
               lastUpdated={
                 data?.updatedAt
-                  ? FaMonument(data.updatedAt).format("Do MMM YYYY")
+                  ? moment(data.updatedAt).format("Do MMM YYYY")
                   : ""
               }
               onSelect={() => navigate(`/interview-prep/${data?._id}`)}
               onDelete={() => setOpenDeleteAlert({ open: true, data })}
-            />;
-          })}
+            />
+          ))}
         </div>
         <button
           className="h-12 md:h12 flex items-center justify-center gap-3 bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 right-10 md-right-20"
