921e0b59a75c82f01c8a24ac461932f895bb92ab
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-29 15:05:16 +0700

    fix: syntax typo

--- frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
+++ frontend/interview-prep-ai/src/pages/Auth/SignUp.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Input from '../../components/Inputs/Input'
-import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector'
-import { validateEmail } from '../../utils/helper';
-import { useContext } from 'react';
-import { UserContext } from '../../context/userContext';
-import axiosInstance from '../../utils/axiosInstance';
-import { API_PATHS } from '../../utils/apiPaths';
-import  uploadImage  from '../../utils/uploadImage';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+import { validateEmail } from "../../utils/helper";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import uploadImage from "../../utils/uploadImage";
 
-const SignUp = ({setCurrentPage}) => {
+const SignUp = ({ setCurrentPage }) => {
   const [profilePic, setProfilePic] = useState(null);
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
@@ -17,7 +17,7 @@ const SignUp = ({setCurrentPage}) => {
 
   const [error, setError] = useState(null);
 
-  const updateUser = useContext(UserContext)
+  const { updateUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
@@ -31,12 +31,12 @@ const SignUp = ({setCurrentPage}) => {
     }
 
     if (!validateEmail(email)) {
-      setError("Please enter a valid email address")
+      setError("Please enter a valid email address");
       return;
     }
 
-    if(!password) {
-      setError("Please enter the password")
+    if (!password) {
+      setError("Please enter the password");
       return;
     }
 
@@ -45,86 +45,85 @@ const SignUp = ({setCurrentPage}) => {
     // Signup API Call
     try {
       if (profilePic) {
-        const imgUploadRes = await uploadImage(profilePic)
-        profileImageUrl = imgUploadRes.imageUrl || ""
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
       }
 
       const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
         name: fullName,
         email,
         password,
-        profileImageUrl
-      })
+        profileImageUrl,
+      });
 
-      const { token } = response.data
+      const { token } = response.data;
 
       if (token) {
-        localStorage.setItem("token", token)
-        updateUser(response.data)
-        navigate("/dashboard")
-      }   
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
     } catch (error) {
       if (error.response && error.response.data.message) {
         setError(error.response.data.message);
       } else {
-        setError("Something went wrong. Please try again.")
+        setError("Something went wrong. Please try again.");
       }
     }
-  }
+  };
 
   return (
-    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
-      <h3 className='text-lg font-semibold text-black'>Create an Account</h3>
-      <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create an Account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
         Join us today by entering your details below.
       </p>
       <form onSubmit={handleSignUp}>
-
         <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
-        <div className=''>
+        <div className="">
           <Input
-            value = {fullName}
-            onChange={({target}) => setFullName(target.value)}
-            label='Full Name'
-            placeholder='John'
-            type='text'
+            value={fullName}
+            onChange={({ target }) => setFullName(target.value)}
+            label="Full Name"
+            placeholder="John"
+            type="text"
           />
 
           <Input
-          value = {email}
-          onChange={({target}) => setEmail(target.value)}
-          label='Email Address'
-          placeholder='John@example.com'
-          type='text'
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="John@example.com"
+            type="text"
           />
 
           <Input
-          value = {password}
-          onChange={({target}) => setPassword(target.value)}
-          label='Password'
-          placeholder='Min 8 Characters'
-          type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
           />
 
-          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 
-          <button type='submit' className='btn-primary'>
+          <button type="submit" className="btn-primary">
             SIGN UP
           </button>
 
-          <p className='text-[13px] text-slate-800 mt-3'>
+          <p className="text-[13px] text-slate-800 mt-3">
             Already have an account?{""}
             <button
-            className='font-medium text-primary underline cursor-pointer'
-            onClick={() => setCurrentPage("login")}
+              className="font-medium text-primary underline cursor-pointer"
+              onClick={() => setCurrentPage("login")}
             >
-            Login
+              Login
             </button>
           </p>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
