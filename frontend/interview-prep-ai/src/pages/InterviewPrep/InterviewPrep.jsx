f63b69ce6efaa06462376d7a67ed08d461abf18b
Bintang Duinata <bintang.duinata31@gmail.com>
2025-07-31 02:38:38 +0700

    feat: function to pin

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -10,48 +10,65 @@ import RoleInfoHeader from '../../components/roleInfoHeader';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import QuestionCard from '../../components/Cards/QuestionCard';
+import AIResponsePreview from '../../components/AIResponsePreview';
+import Drawer from '../../components/Drawer';
 
 const InterviewPrep = () => {
   const { sessionId } = useParams();
 
-  const [ sessionData, setSessionData ] = useState(null);
-  const [ errorMsg, setErrorMsg ] = useState("");
+  const [sessionData, setSessionData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  const [ openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
-  const [ explanation, setExplanation ] = useState(null);
+  const [openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
+  const [explanation, setExplanation] = useState(null);
 
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ isUpdateLoader, setIsUpdateLoader ] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateLoader, setIsUpdateLoader] = useState(false);
 
   // Fetch session data by session id
   const fetchSessionDetailsById = async () => {
-    try{
+    try {
       const response = await axiosInstance.get(
         API_PATHS.SESSION.GET_ONE(sessionId)
       );
 
-      if (response.data && response.data.session){
+      if (response.data && response.data.session) {
         setSessionData(response.data.session);
       }
-    }catch(error){
+    } catch (error) {
       console.error("Error: ", error)
     }
   };
 
   // Generate Concept Explanation
-  const generateConceptExplanation = async (question) => {};
+  const generateConceptExplanation = async (question) => { };
 
   // Pin Question
-  const toggleQuestionPinStatus = async (questionId) => {};
+  const toggleQuestionPinStatus = async (questionId) => {
+    try {
+      const response = await axiosInstance.post(
+        API_PATHS.QUESTION.PIN(questionId)
+      );
+
+      console.log(response)
+
+      if (response.data && response.data.question) {
+        // toast.success('Question Pinned Successfully')
+        fetchSessionDetailsById();
+      }
+    } catch (error) {
+      console.error("Error:", error)
+    }
+  };
 
   // Add more questions to a session
-  const uploadMoreQuestions = async () => {};
+  const uploadMoreQuestions = async () => { };
 
   useEffect(() => {
-    if(sessionId) {
+    if (sessionId) {
       fetchSessionDetailsById();
     }
-    return () => {};
+    return () => { };
   }, []);
   return (
     <DashboardLayout>
@@ -63,8 +80,8 @@ const InterviewPrep = () => {
         description={sessionData?.description || ""}
         lastUpdated={
           sessionData?.updatedAt
-          ? moment(sessionData.updatedAt).format("Do MMM YYYY")
-          : ""
+            ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+            : ""
         }
       />
 
@@ -72,45 +89,58 @@ const InterviewPrep = () => {
         <h2 className='text-lg font-semibold color-black'>Interview Q & A</h2>
         <div className='grid grid-cols-12 gap-4 mt-5 mb-10'>
           <div
-          className={`col-span-12 ${
-            openLeanMoreDrawer ? "md:col-span-7" : "md:col-span-8"
-          }`}
+            className={`col-span-12 ${openLeanMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+              }`}
           >
             <AnimatePresence>
               {sessionData?.questions?.map((data, index) => {
                 return (
                   <motion.div
-                  key={data._id || index}
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 0.95 }}
-                  transition={{
-                    duration: 0.4,
-                    type: "spring",
-                    stiffness: 100,
-                    delay: index * 0.1,
-                    damping: 15,
-                  }}
-                  layout // This is the key prop that animates position changes
-                  layoutId={`question-${data._id || index}`} // Helps framer track specific items
+                    key={data._id || index}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 0.95 }}
+                    transition={{
+                      duration: 0.4,
+                      type: "spring",
+                      stiffness: 100,
+                      delay: index * 0.1,
+                      damping: 15,
+                    }}
+                    layout // This is the key prop that animates position changes
+                    layoutId={`question-${data._id || index}`} // Helps framer track specific items
                   >
                     <>
                       <QuestionCard
                         question={data?.question}
                         answer={data?.answer}
-                        onLearnMore={() => 
+                        onLearnMore={() =>
                           generateConceptExplanation(data.question)
                         }
                         isPinned={data?.isPinned}
                         onTogglePin={() => toggleQuestionPinStatus(data._id)}
                       />
                     </>
-                </motion.div>
+                  </motion.div>
                 )
               })}
-              </AnimatePresence>              
+            </AnimatePresence>
           </div>
         </div>
+        <Drawer
+          isOpen={openLeanMoreDrawer}
+          onClose={() => setOpenLeanMoreDrawer(false)}
+          title={!isLoading && explanation?.title}
+        >
+          {errorMsg && (
+            <p className='flex gap2 text-sm text-amber-600 font-medium'>
+              <LuCircleAlert className='mt-1' /> {errorMsg}
+            </p>
+          )}
+          {!isLoading && explanation && (
+            <AIResponsePreview content={explanation?.explanation} />
+          )}
+        </Drawer>
       </div>
     </DashboardLayout>
   )
