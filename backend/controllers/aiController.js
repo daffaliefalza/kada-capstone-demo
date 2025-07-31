4f23bfbedb961c7fbba381854f3f862264f70097
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-31 16:37:23 +0700

    fix: change model ai

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -63,7 +63,7 @@ const generateConceptExplanation = async (req, res) => {
     const prompt = conceptExplainPrompt(question);
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash-lite",
+      model: "gemini-2.5-flash-lite",
       contents: prompt,
     });
 
