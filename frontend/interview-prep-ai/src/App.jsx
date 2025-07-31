28c877cde09fcfd7ed590baeb62c7669d5404b99
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-31 23:15:32 +0700

    feat: update route

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,38 +1,52 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Home/Dashboard';
-import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
-import UserProvider from './context/userContext';
-
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/qna/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+import UserProvider from "./context/userContext";
+import Features from "./pages/Features";
+import Preview from "./pages/qna/Preview";
+import Error from "./components/Error";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <UserProvider>
       <div>
-      <Router>
-        <Routes>
-          {/* Default Route */}
-          <Route path='/' element={<LandingPage />} />
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path="/" element={<LandingPage />} />
 
-         
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/interview-prep/:sessionId' element={<InterviewPrep />} />
-        </Routes>
-      </Router>
+            {/* Route for not found */}
 
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            fontSize: "13px"
-          },
-        }}
-      />
-    </div>
+            <Route path="*" element={<Error />} />
+            {/* Protected Routes: All routes inside will require authentication */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/features" element={<Features />} />
+              <Route path="/features/qna" element={<Preview />} />
+              <Route path="/features/qna/dashboard" element={<Dashboard />} />
+              <Route
+                path="/interview-prep/:sessionId"
+                element={<InterviewPrep />}
+              />
+              {/* Add any other protected routes here */}
+            </Route>
+          </Routes>
+        </Router>
+
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
     </UserProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
