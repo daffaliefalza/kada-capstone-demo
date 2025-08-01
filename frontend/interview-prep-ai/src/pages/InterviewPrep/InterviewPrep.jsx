9a1b32a7cb8887625b158e117d8226ae3ec817d8
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-08-01 10:38:21 +0700

    feat: update interview prep

--- frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import moment from "moment";
-import { AnimatePresence, motion } from 'framer-motion';
-import { LuCircleAlert, LuListCollapse } from 'react-icons/lu';
-import SpinnerLoader from '../../components/Loader/SpinnerLoader';
-import { toast } from 'react-hot-toast';
-import DashboardLayout from '../../components/Layouts/DashboardLayout';
-import RoleInfoHeader from '../../components/roleInfoHeader';
-import axiosInstance from '../../utils/axiosInstance';
-import { API_PATHS } from '../../utils/apiPaths';
-import QuestionCard from '../../components/Cards/QuestionCard';
-import AIResponsePreview from '../../components/AIResponsePreview';
-import Drawer from '../../components/Drawer';
-import SkeletonLoader from '../../components/Loader/SkeletonLoader';
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
+import RoleInfoHeader from "../../components/roleInfoHeader";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import QuestionCard from "../../components/Cards/QuestionCard";
+import AIResponsePreview from "../../components/AIResponsePreview";
+import Drawer from "../../components/Drawer";
+import SkeletonLoader from "../../components/Loader/SkeletonLoader";
 
 const InterviewPrep = () => {
   const { sessionId } = useParams();
@@ -37,7 +37,7 @@ const InterviewPrep = () => {
         setSessionData(response.data.session);
       }
     } catch (error) {
-      console.error("Error: ", error)
+      console.error("Error: ", error);
     }
   };
 
@@ -45,7 +45,7 @@ const InterviewPrep = () => {
   const generateConceptExplanation = async (question) => {
     try {
       setErrorMsg("");
-      setExplanation(null)
+      setExplanation(null);
 
       setIsLoading(true);
       setOpenLeanMoreDrawer(true);
@@ -61,7 +61,7 @@ const InterviewPrep = () => {
         setExplanation(response.data);
       }
     } catch (error) {
-      setExplanation(null)
+      setExplanation(null);
       setErrorMsg("Failed to generate explanation, Try again later");
       console.error("Error:", error);
     } finally {
@@ -76,38 +76,38 @@ const InterviewPrep = () => {
         API_PATHS.QUESTION.PIN(questionId)
       );
 
-      console.log(response)
+      console.log(response);
 
       if (response.data && response.data.question) {
         // toast.success('Question Pinned Successfully')
         fetchSessionDetailsById();
       }
     } catch (error) {
-      console.error("Error:", error)
+      console.error("Error:", error);
     }
   };
 
   // Add more questions to a session
   const uploadMoreQuestions = async () => {
     try {
-      setIsUpdateLoader(true)
+      setIsUpdateLoader(true);
 
       // Call AI API to generate questions
       const aiResponse = await axiosInstance.post(
-      API_PATHS.AI.GENERATE_QUESTIONS,
-      {
-        role: sessionData?.role,
-        experience: sessionData?.experience,
-        topicsToFocus: sessionData?.topicsToFocus,
-        numberOfQuestions: 10,
-      }
+        API_PATHS.AI.GENERATE_QUESTIONS,
+        {
+          role: sessionData?.role,
+          experience: sessionData?.experience,
+          topicsToFocus: sessionData?.topicsToFocus,
+          numberOfQuestions: 10,
+        }
       );
 
       //  Should be array like [{question, asnwer}, ...]
-      const generatedQuestions = aiResponse.data
+      const generatedQuestions = aiResponse.data;
 
       const response = await axiosInstance.post(
-      API_PATHS.QUESTION.ADD_TO_SESSION,
+        API_PATHS.QUESTION.ADD_TO_SESSION,
         {
           sessionId,
           questions: generatedQuestions,
@@ -119,10 +119,10 @@ const InterviewPrep = () => {
         fetchSessionDetailsById();
       }
     } catch (error) {
-      if (error.response && error.response.data.message){
-        setErrorMsg(error.response)
+      if (error.response && error.response.data.message) {
+        setErrorMsg(error.response);
       } else {
-      setError("Something went wrong. Please try again.")
+        setError("Something went wrong. Please try again.");
       }
     } finally {
       setIsUpdateLoader(false);
@@ -133,7 +133,7 @@ const InterviewPrep = () => {
     if (sessionId) {
       fetchSessionDetailsById();
     }
-    return () => { };
+    return () => {};
   }, []);
   return (
     <DashboardLayout>
@@ -150,12 +150,13 @@ const InterviewPrep = () => {
         }
       />
 
-      <div className='container mx-auto pt-4 pb-4 px-4 md:px-0'>
-        <h2 className='text-lg font-semibold color-black'>Interview Q & A</h2>
-        <div className='grid grid-cols-12 gap-4 mt-5 mb-10'>
+      <div className="container mx-auto pt-4 pb-4 px-4 md:px-0">
+        <h2 className="text-lg font-semibold color-black">Interview Q & A</h2>
+        <div className="grid grid-cols-12 gap-4 mt-5 mb-10">
           <div
-            className={`col-span-12 ${openLeanMoreDrawer ? "md:col-span-7" : "md:col-span-8"
-              }`}
+            className={`col-span-12 ${
+              openLeanMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+            }`}
           >
             <AnimatePresence>
               {sessionData?.questions?.map((data, index) => {
@@ -189,23 +190,23 @@ const InterviewPrep = () => {
 
                     {!isLoading &&
                       sessionData?.questions?.length == index + 1 && (
-                        <div className='flex items-center justify-center mt-5'>
+                        <div className="flex items-center justify-center mt-5">
                           <button
-                            className='flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer'
+                            className="flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer"
                             disabled={isLoading || isUpdateLoader}
                             onClick={uploadMoreQuestions}
                           >
                             {isUpdateLoader ? (
                               <SpinnerLoader />
                             ) : (
-                              <LuListCollapse className='text-lg' />
+                              <LuListCollapse className="text-lg" />
                             )}{" "}
                             Load More
                           </button>
                         </div>
                       )}
                   </motion.div>
-                )
+                );
               })}
             </AnimatePresence>
           </div>
@@ -216,8 +217,8 @@ const InterviewPrep = () => {
           title={!isLoading && explanation?.title}
         >
           {errorMsg && (
-            <p className='flex gap2 text-sm text-amber-600 font-medium'>
-              <LuCircleAlert className='mt-1' /> {errorMsg}
+            <p className="flex gap2 text-sm text-amber-600 font-medium">
+              <LuCircleAlert className="mt-1" /> {errorMsg}
             </p>
           )}
           {isLoading && <SkeletonLoader />}
@@ -227,7 +228,7 @@ const InterviewPrep = () => {
         </Drawer>
       </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default InterviewPrep
+export default InterviewPrep;
