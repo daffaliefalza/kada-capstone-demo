76175144f325614c6cd87878e39eae065a8070c8
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-08-04 12:10:12 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const resumeRoutes = require("./routes/resumeRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
+const codingRoutes = require("./routes/codingRoutes");
 const { protect } = require("./middlewares/authMiddleware.js");
 const {
   generateInterviewQuestion,
@@ -42,7 +43,7 @@ app.use("/api/ai/generate-questions", protect, generateInterviewQuestion);
 app.use("/api/ai/generate-explanations", protect, generateConceptExplanation);
 
 app.use("/api/resumes", resumeRoutes); // Use the routes
-
+app.use("/api/coding", codingRoutes);
 // serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
